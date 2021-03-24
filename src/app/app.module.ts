5e63813af88a5526cbe51cdfd4a0885dfedd5672
Bhargava Chittapudi <Bhargava_Chittapudi@epam.com>
2021-03-24 23:21:40 +0530

    initial implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { ChartsModule } from 'ng2-charts';
+import { DoughnutChartComponent } from './doughnut-chart/doughnut-chart.component';
+import { NgModule } from '@angular/core';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
+import { RadarChartComponent } from './radar-chart/radar-chart.component';
+
+const routes: Routes = [
+  { path: 'bar-chart', component: BarChartComponent },
+  { path: 'doughnut-chart', component: DoughnutChartComponent },
+  { path: 'radar-chart', component: RadarChartComponent },
+  { path: 'pie-chart', component: PieChartComponent },
+  { path: '**', component: BarChartComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarChartComponent,
+    DoughnutChartComponent,
+    RadarChartComponent,
+    PieChartComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
